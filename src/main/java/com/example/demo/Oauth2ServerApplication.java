20c50e24b68a2214ea0a4cffc64227bd647a35c0
zhangcl2002 <chunlinzhang@msn.com>
2017-12-04 22:47:08 +0800

    ss

--- src/main/java/com/example/demo/Oauth2ServerApplication.java
+++ src/main/java/com/example/demo/Oauth2ServerApplication.java
@@ -4,7 +4,12 @@ import java.security.Principal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -110,5 +115,25 @@ public class Oauth2ServerApplication extends AuthorizationServerConfigurerAdapte
         .refreshTokenValiditySeconds(60*60*24); // 24h  
     }
 	
+    @Configuration  
+    @EnableWebSecurity  
+    static class SecurityConfig extends WebSecurityConfigurerAdapter {  
+        @Override  
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {  
+            auth.inMemoryAuthentication()  
+                .withUser("user").password("123").roles("USER")  
+                .and()  
+                .withUser("admin").password("123").roles("ADMIN");  
+        }  
+  
+        @Override  
+        protected void configure(HttpSecurity http) throws Exception {  
+            http.csrf().disable();  
+            http.authorizeRequests()  
+                .antMatchers("/oauth/authorize").authenticated()  
+                .and()  
+                .httpBasic().realmName("OAuth Server");  
+        }  
+    }  
 	
 }
---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import java.io.UnsupportedEncodingException;
+
+import org.springframework.security.crypto.codec.Base64;
+
+
+public class Test {  
+	  
+    public static void main(String[] args) {  
+        System.out.println(generate("oauth_client", "oauth_client_secret"));  
+    }  
+  
+    private static String generate(String clientId, String clientSecret) {  
+        String creds = String.format("%s:%s", new Object[] { clientId, clientSecret });  
+        try {  
+            return "Basic " + new String(Base64.encode(creds.getBytes("UTF-8")));  
+        } catch (UnsupportedEncodingException e) {  
+            throw new IllegalStateException("Could not convert String");  
+        }  
+    }  
+  
+}  
